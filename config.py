normalization fixed, reward fixed

config.py
@@ -3,17 +3,20 @@ import numpy as np
 
 W, H = 600, 760
 
-POWER_LEVELS = [0., .5, .6, .7, .8, 1.]
-ANGLE_LEVELS = [-np.pi/6, -np.pi/18, 0, np.pi/18, np.pi/6]
+POWER_LEVELS = [0., .5, 1.]
+ANGLE_LEVELS = [-np.pi/6, 0, np.pi/6]  # pi/18
 ACTION = list(product(ANGLE_LEVELS, POWER_LEVELS))
 
 EPS_START = 0.9
 EPS_END = 0.05
-EPS_DECAY = 400
+EPS_DECAY = 200  # 400
+TARGET_UPDATE = 10
 
 MEMORY_SZ = 10000
-MINIBATCH = 40
+MINIBATCH = 50
+LR = 1e-5
 
 # (x, y, r, vx, vy, w, x_prop)
-# STATE_MINMAX = [(0, 40), (-600 / 2, 600 / 2), (0, 760), (-np.pi/3, np.pi/3), (-40, 40), (-120, 50), (-1.2, 1.2)]
-STATE_MINMAX = [(-W/2, W/2), (0, H), (-np.pi/2, np.pi/2), (-40, 40), (-110, 50), (-np.pi/6, np.pi/6), (0, 50)]
+# STATE_MINMAX = [(-W/2, W/2), (0, H), (-pi/2, pi/2), (-40, 40), (-110, 50), (-np.pi/6, np.pi/6), (0, 50)]
+# STATE_MINMAX = [(-W/2, W/2), (0, H), (0, 1), (-40, 40), (-110, 50), (0, 1), (0, 50)]
+STATE_MINMAX = [(0, W/2), (0, H), (0, 1), (0, W/2), (0, H), (0, 1), (0, 50)]

src/dql.py
@@ -1,10 +1,8 @@
 from src.render import Visualization
 from src.rocket import Rocket
 from random import uniform as uf
-from src.tools import normalize_state
 from copy import deepcopy
 import torch.nn as nn
-import numpy as np
 import torch
 import random
 import pickle
@@ -26,7 +24,6 @@ class DQL:
 
         # hyperparameter dql
         self.gamma = 0.99
-        self.number_of_samples = 100000
 
         # physics
         self.rocket = Rocket(self.w, self.h, prop=0.5)
@@ -39,7 +36,7 @@ class DQL:
             self.policy_net.layers = self.policy_net.layers.to(self.device)
             self.target_net.layers = self.target_net.layers.to(self.device)
         self.target_net.layers.load_state_dict(self.policy_net.get_state())
-        self.optim = torch.optim.Adam(self.policy_net.parameters(), lr=3e-4)
+        self.optim = torch.optim.Adam(self.policy_net.parameters(), lr=LR)
         self.crit = torch.nn.MSELoss()
 
         # rendering
@@ -47,15 +44,21 @@ class DQL:
             self.render = Visualization(600, 760)
 
     def iteration(self, m):
+        # keep stats
+        losses = []
         scores = []
+
         for game in range(m):
             eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * game / EPS_DECAY)
 
             # randomly set state_0, reset score
+            game_duration = 0
             score = 0
+            loss = 0
             self.rocket.set_state(uf(-100, 100), uf(650, 750), uf(-0.2, 0.2), uf(-20, 20), uf(-120, -80), uf(-0.2, 0.2))
             state_0 = torch.tensor(self.rocket.get_state(), device=self.device).float()
             while not self.rocket.dead:
+                game_duration += 1
                 # select next action
                 if random.random() < eps_threshold:
                     action_idx = random.randint(0, len(ACTION)-1)
@@ -79,18 +82,23 @@ class DQL:
                 self.memory.push(transition, action_idx)
 
                 # train minibatch
-                self.train()
+                loss += self.train()
                 state_0 = torch.tensor(transition[3], device=self.device).float()
 
-            self.score.append(score)
-            print('game: {} score: {:.2f} mean: {:.2f} eps: {:.2f}'.format(game, score,
-                                                                           sum(self.score) / len(self.score),
-                                                                           eps_threshold))
+            # stats
+            losses.append(loss / game_duration)
+            scores.append(score)
+
+            mean_loss = sum(scores[-100:]) / len(scores[-100:])
+            print('game: {} score: {:.2f} mean: {:.2f} loss: {:.3f} eps: {:.2f}'.format(game, score, mean_loss,
+                                                                                        losses[-1], eps_threshold))
+
             # save state, update target net weights
-            if (game+1) % 1000 == 0:
+            if (game+1) % 100 == 0:
+                dump = {'scores': scores, 'losses': losses}
                 self.policy_net.save_state()
-                pickle.dump(scores, open('runs/scores.pkl', 'wb'))
-            if game % 10 == 0:
+                pickle.dump(dump, open('runs/stats.pkl', 'wb'))
+            if game % TARGET_UPDATE == 0:
                 self.target_net.layers.load_state_dict(deepcopy(self.policy_net.get_state()))
 
     def train(self):
@@ -108,8 +116,9 @@ class DQL:
         self.optim.zero_grad()
         loss = self.crit(q_target, q_eval)
         loss.backward()
-        nn.utils.clip_grad_value_(self.policy_net.parameters(), 1)
+        nn.utils.clip_grad_norm_(self.policy_net.parameters(), 1.0)
         self.optim.step()
+        return loss.item()/state_0_batch.shape[0]
 
     def test(self, state_dict=None):
         if state_dict is not None:
@@ -132,18 +141,22 @@ class DQL:
 class DQLModel(nn.Module):
     def __init__(self):
         super().__init__()
+        self.input_sz = 7  # state dim = 7
+        self.output_sz = len(ACTION)
+
         self.layers = nn.Sequential()
-        self.layers.add_module('input', nn.Linear(7, 128))  # state is 7 dim
-        self.layers.add_module('ai', nn.Tanh())
-        self.layers.add_module('h1', nn.Linear(128, 128))
-        self.layers.add_module('a1', nn.ReLU())
+        self.layers.add_module('input', nn.Linear(self.input_sz, 128))
+
+        self.layers.add_module('h1', nn.Linear(128, 128))  # (100, 200)
+        self.layers.add_module('a1', nn.Sigmoid())
+
+        self.layers.add_module('h2', nn.Linear(128, 128))
+        self.layers.add_module('a2', nn.Sigmoid())
+
         # self.layers.add_module('h2', nn.Linear(128, 128))
         # self.layers.add_module('a2', nn.ReLU())
-        self.layers.add_module('h3', nn.Linear(128, 90))
-        self.layers.add_module('a3', nn.ReLU())
-        self.layers.add_module('h4', nn.Linear(90, 70))
-        self.layers.add_module('a4', nn.ReLU())
-        self.layers.add_module('out', nn.Linear(70, len(ACTION)))
+
+        self.layers.add_module('out', nn.Linear(128, self.output_sz))
 
         self.layers.apply(init_weights)
 
@@ -186,18 +199,13 @@ class Memory:
         state_0_batch, state_1_batch, action_batch, reward_batch = [], [], [], []
         terminated = []
         for t in batch:
-            # norm state
-            s0, s1 = t[0], t[3]
-            state_0 = normalize_state(t[0])
-            state_1 = normalize_state(t[3])
-            # for j in range(s0.shape[0]):
-            #     s0[j] = norm_minmax(s0[j], STATE_MINMAX[j])
-            #     s1[j] = norm_minmax(s1[j], STATE_MINMAX[j])
-            state_0_batch.append(state_0)
-            state_1_batch.append(state_1)
+            # normalize both states same as in reward function
+            # bulid batches for training
+            state_0_batch.append(t[0])
             action_batch.append(t[1])
             reward_batch.append(t[2])
-            terminated.append(t[-1])
+            state_1_batch.append(t[3])
+            terminated.append(t[4])
         state_0_batch = torch.tensor(state_0_batch, device=self.device).float()
         action_batch = torch.stack(action_batch).float().to(self.device)
         state_1_batch = torch.tensor(state_1_batch, device=self.device).float()
@@ -209,4 +217,4 @@ class Memory:
 def init_weights(m):
     if type(m) == nn.Linear:
         torch.nn.init.uniform_(m.weight, -0.01, 0.01)
-        m.bias.data.fill_(0)
+        m.bias.data.fill_(0.01)

src/rocket.py
@@ -1,4 +1,3 @@
-from src.tools import normalize_state
 from numpy.linalg import norm
 from math import fmod
 import numpy as np
@@ -37,10 +36,11 @@ class Rocket:
         self.moment_of_inertia()
 
         # position and thruster
-        self.f_0 = 9 * 420e3  # maximal Thrust in Newton, 9 Merlin C1
+        self.f_0 = 9 * 540e3  # maximal Thrust in Newton, 9 Merlin C1 (before: 9*420e3)
         self.prop_consumption = (self.length*self.cross_sec)/(self.f_0*170/self.t_step) * 10  # m^3/N, 170s burntime
         self.position = [np.array([0, 200, 0])]  # [np.zeros(3)]  # coordinates + rotation (x, y, R)
         self.thruster = np.zeros(2)  # angle + force (phi, f) (local coordinates)
+        self.thruster_level = 0
 
         self.omega = np.array([0])
         self.v = np.array([[random.randint(-10, 10)],
@@ -53,6 +53,7 @@ class Rocket:
         self.lifespan += 1
 
         # upadte thruster
+        self.thruster_level = power
         self.update_thrust(angle, power)
 
         # update propellant and mass
@@ -77,30 +78,22 @@ class Rocket:
 
     def update_score(self):
         # depends on velocity, rotation, distance to 0
-        state = normalize_state(self.get_state())
-        reward = 0
-
-        # -100 * distance to landing pad, -100 * velocity, -100 * angle
-        shaping = - 100 * norm(state[0:1]) \
-                  - 100 * norm(state[3:4]) \
-                  - 100 * abs(state[2])
-
-        # shaping = -10 * norm(self.position[-1][0:1]) \
-        #           - 100 * norm(self.v)\
-        #           - 100 * abs(self.position[-1][2])
+        state = self.get_state()
+        reward = np.array([0])
+        # -100 * distance to landing pad, -100 * velocity, -100 * angle, -100 * omega
+        shaping = np.array([- 100 * norm(state[0:2]),  # distance
+                            - 200 * norm(state[3:5]),
+                            - 100 * norm(state[2])])  # velocity
 
         if self.previous_shaping is not None:
             reward = shaping - self.previous_shaping
         self.previous_shaping = shaping
+        reward = reward.sum()
+        reward -= 0.1 * abs(state[2])
 
-        """
-        sc = -10*(norm([x, y]) - norm([xl, yl])) - 100*(norm([vx, vy]) - norm([vxl, vyl])) \
-             - 100*(w - wl) - 10*abs(r)
-
-        sc *= 0.8 + (1.5 - 0.8) * np.exp(- abs(r) * 5)
-        """
+        # reward -= (0.1 + (1 - 0.1) * np.exp(- norm(state[0:1])/.5)) * norm(state[3:4])
         if self.success:
-            reward += 100
+            reward += 200
 
         return reward
 
@@ -170,8 +163,15 @@ class Rocket:
 
     """ *********** Get / Set *********** """
     def get_state(self):
-        return np.concatenate([self.position[-1].ravel(), self.v.ravel(), self.omega.ravel(),
-                               np.array([self.x_prop])], axis=0)
+        state = np.concatenate([self.position[-1][0].ravel()/(self.w/2),  # x
+                                self.position[-1][1].ravel()/self.h,  # y
+                                self.position[-1][2].ravel(),  # r
+                                self.v[0].ravel()/(self.w/2)*self.t_step * 100,  # vx
+                                self.v[1].ravel()/self.h*self.t_step * 100,  # vy
+                                self.omega.ravel()/self.t_step*20,  # omega
+                                np.array([self.x_prop]).ravel()/40])  # c_prop
+
+        return state  # normalize_state(state)
 
     def set_state(self, x, y, r, v_x, v_y, w):
         self.position.append(np.array([x, y, r]))